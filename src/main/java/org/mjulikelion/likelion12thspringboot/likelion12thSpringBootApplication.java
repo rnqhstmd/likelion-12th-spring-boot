be14af581d3a9c7a77dda470395d96242df518f6
bonseung <bs.koo@sqisoft.com>
2024-04-13 16:33:30 +0900

    first commit

---
+++ src/main/java/org/mjulikelion/likelion12thspringboot/likelion12thSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelion12thspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class likelion12thSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(likelion12thSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mjulikelion/likelion12thspringboot/Likelion12thSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mjulikelion.likelion12thspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Likelion12thSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
